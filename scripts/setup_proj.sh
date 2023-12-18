5752e791d4def30b6f48dec323d3509a750f960d
shivashankarv <varunshivashankardev@gmail.com>
2023-12-18 11:22:02 -0500

    Added git setup and auto git push

--- scripts/setup_proj.sh
+++ scripts/setup_proj.sh
@@ -2,11 +2,17 @@
 
 PROJECTNAME=$1
 
+cd ../
+
 sed -i 's/pytemplate/${PROJECTNAME}/g' environment.yml
 sed -i 's/pytemplate/${PROJECTNAME}/g' pyproject.toml
 mv pytemplate $PROJECTNAME
 
+conda env create -f environment.yml
+eval "$(conda shell.bash hook)"
+conda activate $PROJECTNAME
+pip install -e .
+
 git init
 git commit -am "Setting up the project"
-
-conda env create -f environment.yml
+git push
