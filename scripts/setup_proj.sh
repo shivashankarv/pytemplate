3489880b99337e04a5d86a9a330fc97c3e8f5e5c
shivashankarv <varunshivashankardev@gmail.com>
2023-12-18 12:00:02 -0500

    Setting up the project

--- scripts/setup_proj.sh
+++ scripts/setup_proj.sh
@@ -1,12 +1,14 @@
 # Script that sets up the project
 
 PROJECTNAME=$1
+echo "Setting up ${PROJECTNAME}"
 
 cd ../
+pwd
 
+mv pytemplate $PROJECTNAME
 sed -i 's/pytemplate/${PROJECTNAME}/g' environment.yml
 sed -i 's/pytemplate/${PROJECTNAME}/g' pyproject.toml
-mv pytemplate $PROJECTNAME
 
 conda env create -f environment.yml
 eval "$(conda shell.bash hook)"
