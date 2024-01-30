94e378099cf4d9bdfa75f68c7534002d5fdcf62d
shivashankarv <varunshivashankardev@gmail.com>
2024-01-30 12:14:54 -0500

    Unknown commit

--- scripts/setup_proj.sh
+++ scripts/setup_proj.sh
@@ -1,18 +1,21 @@
 # Script that sets up the project
 
 PROJECTNAME=$1
+echo "Setting up ${PROJECTNAME}"
 
 cd ../
+pwd
 
-sed -i 's/pytemplate/${PROJECTNAME}/g' environment.yml
-sed -i 's/pytemplate/${PROJECTNAME}/g' pyproject.toml
+sed -i '' "s@pytemplate@${PROJECTNAME}@g" environment.yml
+sed -i '' "s@pytemplate@${PROJECTNAME}@g" pyproject.toml
 mv pytemplate $PROJECTNAME
 
-conda env create -f environment.yml
+which conda
+mamba env create -f environment.yml
 eval "$(conda shell.bash hook)"
-conda activate $PROJECTNAME
+condaa activate "$PROJECTNAME"
 pip install -e .
 
-git init
-git commit -am "Setting up the project"
-git push
+#git init
+#git commit -am "Setting up the project"
+#git push
